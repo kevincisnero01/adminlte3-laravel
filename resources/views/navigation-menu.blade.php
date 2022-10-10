401c792a4a27a2dadfb8744c5a40ff6a5eba3145
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2022-10-10 17:23:31 -0400

    Component  Jetstream

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,8 +1,8 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
+        <div class="flex h-16" id="navitaizo">
+            <div class="flex flex-1 ">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
@@ -11,13 +11,23 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex flex-1">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        <i class="fas fa-tasks-alt mr-1"></i>
                         {{ __('Dashboard') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <!-- Login Links -->
+                <div class="hidden sm:-my-px sm:ml-4 sm:flex ">
+                    <x-jet-nav-link href="/login">
+                        <i class="fa-solid fa-right-to-bracket mr-1"></i>
+                        {{ __('Login') }}
                     </x-jet-nav-link>
                 </div>
             </div>
 
+            @auth   
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <!-- Teams Dropdown -->
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
@@ -68,9 +78,10 @@
                         </x-jet-dropdown>
                     </div>
                 @endif
-
+                
                 <!-- Settings Dropdown -->
                 <div class="ml-3 relative">
+                
                     <x-jet-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
@@ -80,8 +91,8 @@
                             @else
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
-                                        {{ Auth::user()->name }}
-
+                                         <!--Auth::user()->name-->
+                                            Kevin Cisnero
                                         <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                         </svg>
@@ -119,9 +130,10 @@
                             </form>
                         </x-slot>
                     </x-jet-dropdown>
+                
                 </div>
             </div>
-
+             @endauth
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
@@ -133,7 +145,7 @@
             </div>
         </div>
     </div>
-
+    {{-- 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
@@ -141,9 +153,10 @@
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
         </div>
-
+        
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
+            
             <div class="flex items-center px-4">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                     <div class="shrink-0 mr-3">
@@ -152,11 +165,11 @@
                 @endif
 
                 <div>
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    <div class="font-medium text-base text-gray-800"> <!-- Auth::user()->name --> </div>
+                    <div class="font-medium text-sm text-gray-500"> <!-- Auth::user()->email } --> </div>
                 </div>
             </div>
-
+            
             <div class="mt-3 space-y-1">
                 <!-- Account Management -->
                 <x-jet-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
@@ -212,4 +225,5 @@
             </div>
         </div>
     </div>
+    --}}
 </nav>
